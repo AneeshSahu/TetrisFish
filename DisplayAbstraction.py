91759d30e1c17b4ae7398d85b4680e80ba76a8a2
AneeshSahu <aneesh2115@gmail.com>
2023-09-15 23:32:18 -0400

    replaced tkinter with pygame

--- DisplayAbstraction.py
+++ DisplayAbstraction.py
@@ -1,68 +1,82 @@
-import tkinter as tk
+import pygame
+import time
 
 
-class DisplayAbstraction(tk.Tk):
+class DisplayAbstraction():
     def __init__(self):
-        super().__init__()
-        self.canvas = tk.Canvas(self, width=500, height=1000)
-        self.canvas.pack()
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((500,1000))
+
         self.shape = (10, 22)
         self.board = [0 for i in range(self.shape[0] * self.shape[1])]
-        self.objects = None
-        self.counter = 0 # counts up to a second in ticks
-        # self.draw_rectangle()
+        self.objects = []
+        self.objects.append(pygame.draw.rect(self.screen, (255, 255, 255), (0, 0, 500, 1000)))
+        self.board[self.shape[0]*2 + 1] = 1
 
-        self.setup()
+        self.clock = pygame.time.Clock()
+        self.tickrate = 60
 
-    def setup(self):
-        self.board[0] = 1
+        #self.counter = 0 # counts up to a second in ticks
+        #self.lastframe = 0
 
-        self.bind("<Right>", self.Keypress)
-        self.bind("<Left>", self.Keypress)
-        self.bind("<Up>", self.Keypress)
-        self.bind("<Down>", self.Keypress)
 
-        self.tick()
 
-    def draw_rectangle(self, x1, x2, y1, y2):
-        self.objects = self.canvas.create_rectangle(x1, y2, x1, y2, outline="black", fill="red")
-
-    # def delete_rectangle(self,event):
-    #   self.canvas.delete(self.objects[-1])
-    #   self.objects.pop()
     def Keypress(self, event):
+        print(event['scancode'])
         found = self.board.index(1)
-        if event == "<Down>" or event.keycode == 40 :  # Down
-            self.board[found + self.shape[0] ], self.board [found] = 1, 0
-        elif event.keycode == 39:  # Right
-            self.board[found + 1 ], self.board [found] = 1, 0
+        if event == "<Down>" or event['scancode'] == 81 :  # Down
+            self.Move("Down")
+        elif event['scancode'] == 79:  # Right
+            #self.board[found + 1 ], self.board [found] = 1, 0
             #self.canvas.move(self.objects, 50, 0)
-        elif event.keycode == 37:  # Left
-            self.board[found - 1 ], self.board [found] = 1, 0
-        elif event.keycode == 38:  # Up
+            self.Move("Right")
+        elif event['scancode'] == 80:  # Left
+            #self.board[found - 1 ], self.board [found] = 1, 0
+            self.Move("Left")
+        elif event['scancode'] == 82:  # Up
             self.board[found - self.shape[0] ], self.board [found] = 1, 0
 
-    def tick(self):
-        self.counter += 1
-        if self.counter == 20:
-            self.counter = 0
-        if self.counter % 10 ==0:
-            self.Keypress("<Down>");
-        self.draw()
-        self.after(50, self.tick)
+    def Move(self, direction):
+        if direction == "Down":
+            for x in range(self.shape[0]):
+                for y in range(self.shape[1]-1, -1, -1):
+                    if self.board[x + y * self.shape[0]] == 1:
+                        self.board[x + (y+1) * self.shape[0]], self.board[x + y * self.shape[0]] = 1, 0
+        elif direction == "Right" :
+            for x in range(self.shape[0]-1, -1, -1):
+                for y in range(self.shape[1]):
+                    if self.board[x + y * self.shape[0]] == 1:
+                        self.board[x+1 + y * self.shape[0]], self.board[x + y * self.shape[0]] = 1, 0
+        elif direction == "Left" :
+            for x in range(self.shape[0]):
+                for y in range(self.shape[1]):
+                    if self.board[x + y * self.shape[0]] == 1:
+                        self.board[x-1 + y * self.shape[0]], self.board[x + y * self.shape[0]] = 1, 0
+
     def draw(self):
         for x in range(self.shape[0]):
             for y in range(self.shape[1]):
                 if self.board[x + y * self.shape[0]] == 1:
-                    colour = "red"
+                    colour = (255,0,0)
                 else:
-                    colour = "white"
-                self.canvas.create_rectangle(x * 50, (y-2) * 50, x * 50 + 50, (y-2) * 50 + 50, outline="black", fill=colour)
+                    colour = (255,255,255)
+                pygame.draw.rect(self.screen, colour,(x * 50, (y-2) * 50, x * 50 + 50, (y-2) * 50 + 50))
+    def run (self):
+        while (True):
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                elif event.type == pygame.KEYDOWN:
+                    self.Keypress(event.dict)
+            if pygame.time.get_ticks() % (1000 // self.tickrate) == 0:
+                self.draw()
+            pygame.display.flip()
+            self.clock.tick(self.tickrate)
 
-    def start(self):
-        self.mainloop()
 
 
 
 dis = DisplayAbstraction()
-dis.start()
+dis.run()
