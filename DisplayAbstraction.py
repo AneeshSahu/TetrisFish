1d7f7ad6c6999d7c5071cd69807618a3de2c78a6
AneeshSahu <aneesh2115@gmail.com>
2023-09-21 23:42:51 -0400

    Finished tetris game

--- DisplayAbstraction.py
+++ DisplayAbstraction.py
@@ -5,30 +5,34 @@ import time
 
 
 class DisplayAbstraction():
-    tetronome = {0: "L", 1: "J", 2: "T", 3: "S" , 4: "Z", 5 : "I", 6: "O"}
-    tetronomeShape = ((4,5,6,14),(4,5,6,16),(4,5,6,15),(5,6,14,15),(4,5,15,16),(3,4,5,6),(4,5,14,15))
+    tetronome = {0: "L", 1: "J", 2: "T", 3: "S", 4: "Z", 5: "I", 6: "O"}
+    tetronomeShape = (
+        (4, 5, 6, 14), (4, 5, 6, 16), (4, 5, 6, 15), (5, 6, 14, 15), (4, 5, 15, 16), (3, 4, 5, 6), (4, 5, 14, 15))
     spawnpoint = 3
 
     def __init__(self):
         pygame.init()
 
-        self.screen = pygame.display.set_mode((500,1000))
+        self.screen = pygame.display.set_mode((500, 1000))
 
         self.shape = (10, 22)
         self.board = [0 for i in range(self.shape[0] * self.shape[1])]
         self.objects = []
         self.objects.append(pygame.draw.rect(self.screen, (255, 255, 255), (0, 0, 500, 1000)))
         self.currentTetronome = 0
+        self.spawnBag = []
+        self.currentOrientation = 0
         self.clock = pygame.time.Clock()
-        self.tickrate = 100 # ms
+        self.tickrate = 100  # ms
+        self.score = 0
 
         self.SpawnATetronome()
 
-        #self.counter = 0 # counts up to a second in ticks
-        #self.lastframe = 0
+        # self.counter = 0 # counts up to a second in ticks
+        # self.lastframe = 0
 
-    def CheckLegality(self,direction):
-        start, end,step = 0,0,0
+    def CheckLegality(self, direction):
+        start, end, step = 0, 0, 0
         move = 0
         if direction == "Right":
             start = self.shape[0] - 1
@@ -48,9 +52,9 @@ class DisplayAbstraction():
         for i in range(start, end, step):
             if self.board[i] == 1:
                 return False
-        for i in range(0, self.shape[0]*self.shape[1]):
+        for i in range(0, self.shape[0] * self.shape[1]):
             if self.board[i] == 1:
-                if i + move >= self.shape[0]*self.shape[1] or i + move < 0:
+                if i + move >= self.shape[0] * self.shape[1] or i + move < 0:
                     return False
                 elif self.board[i + move] == 2:
                     return False
@@ -64,31 +68,178 @@ class DisplayAbstraction():
         elif event['scancode'] == 80 and self.CheckLegality("Left"):  # Left
             self.Move("Left")
         elif event['scancode'] == 82:  # Up
-            found = self.board.index(1)
-            self.board[found - self.shape[0] ], self.board [found] = 1, 0
+            # found = self.board.index(1)
+            # self.board[found - self.shape[0] ], self.board [found] = 1, 0
+            self.rotate()
         elif event['scancode'] == 44:  # Space
-            while self.CheckLegality("Down"):
-                self.Move("Down")
+            self.moveToEnd()
+
+    def moveToEnd(self):
+        while self.CheckLegality("Down"):
+            self.MoveCurrentBlockDown()
+        self.MoveCurrentBlockDown() # extra to take block from 1 to 2
+
+    def rotate(self):
+        if self.DoRotate(False):
+            self.DoRotate(True)
+
+    def DoRotate(self, move=False):
+        # {0: "L", 1: "J", 2: "T", 3: "S" , 4: "Z", 5 : "I", 6: "O"}
+        ret = True
+        found = self.board.index(1)
+        newPeice = []
+        oldPeice = []
+        newOrientation = self.currentOrientation
+        if self.currentTetronome == 0:
+            if self.currentOrientation == 0:
+                oldPeice = [found, found+1,found+2,found+self.shape[0]]
+                newPeice = [found-self.shape[0],found-self.shape[0] + 1 ,found+1, found+1+self.shape[0]]
+                newOrientation = 1
+            if self.currentOrientation == 1:
+                oldPeice = [found, found + 1, found+self.shape[0]+1, found+2*self.shape[0]+1]
+                newPeice = [found+2, found+2 + self.shape[0],found+1 + self.shape[0],found+ self.shape[0] ]
+                newOrientation = 2
+                if found % 10 == 8 : # last row:
+                    newPeice = [x-1 for x in newPeice]
+                    #rerange cause else stuff gets deleted
+                    newPeice[0], newPeice[1], newPeice[2] = newPeice[2], newPeice[0], newPeice[1]
+                pass
+            if self.currentOrientation == 2:
+                # coded this wrong. Fix :
+                found = found - 2 + self.shape[0]
+                oldPeice = [found,found+1, found+2, found+2 -self.shape[0]]
+                newPeice = [found + 1 - self.shape[0] ,found + 1, found + 1 + self.shape[0] , found +2 +self.shape[0]]
+                newOrientation = 3
+                pass
+            if self.currentOrientation == 3:
+                oldPeice = [found,found+self.shape[0],found+2*self.shape[0],found+2*self.shape[0]+1]
+                if found % 10 == 0: # first row
+                    found+=1
+                newPeice = [found+self.shape[0] +1, found+self.shape[0], found+self.shape[0] -1, found+2*self.shape[0]-1 ]
+                newOrientation = 0
+                pass
+        elif self.currentTetronome == 1:
+            if self.currentOrientation == 0 :
+                oldPeice= [found, found+1,found+2,found+2+self.shape[0]]
+                newPeice= [found+1 - self.shape[0],found+1,found+1 + self.shape[0],found + self.shape[0] ]
+                newOrientation = 1
+            elif self.currentOrientation == 1 :
+                oldPeice= [found, found+self.shape[0] , found+2*self.shape[0], found+2*self.shape[0]-1]
+                if found % 10 == 9:
+                    found -=1
+                newPeice= [found-1,found+self.shape[0]-1,found+self.shape[0],found+self.shape[0]+1 ]
+                newOrientation = 2
+            elif self.currentOrientation == 2 :
+                oldPeice= [found,found+self.shape[0],found+self.shape[0]+1, found+self.shape[0]+2]
+                newPeice= [found+1, found+2,found+self.shape[0]+1,found+2*self.shape[0]+1]
+                newOrientation = 3
+            elif self.currentOrientation == 3 :
+                oldPeice= [found+self.shape[0],found,found+1,found+2*self.shape[0]]
+                if found % 10 == 0:
+                    found+=1
+                newPeice= [found+2*self.shape[0]+1,found+self.shape[0]+1,found+self.shape[0],found+self.shape[0]-1 ]
+                newOrientation = 0
+        elif self.currentTetronome == 2:
+            if self.currentOrientation == 0:
+                oldPeice = [found, found+1, found+2, found+1+self.shape[0]]
+                newPeice = [found, found+1 , found+1 +self.shape[0], found-self.shape[0]+1]
+                newOrientation = 1
+            elif self.currentOrientation == 1:
+                oldPeice = [found,found+self.shape[0], found+ self.shape[0]-1, found+2*self.shape[0]  ]
+                if found%10 == 9:
+                    found -=1
+                newPeice = [found,found+self.shape[0],found+self.shape[0]-1,found+self.shape[0]+1]
+                newOrientation = 2
+            elif self.currentOrientation == 2:
+                oldPeice = [found, found +self.shape[0],found +self.shape[0] - 1, found + 1 + self.shape[0]]
+                newPeice = [found, found + self.shape[0], found + 1 + self.shape[0], found +2*self.shape[0]]
+                newOrientation = 3
+            elif self.currentOrientation == 3:
+                oldPeice = [found,found+self.shape[0],found+2*self.shape[0],found+self.shape[0]+1]
+                if found%10 == 0:
+                    found +=1
+                newPeice = [found+self.shape[0],found+self.shape[0]-1, found+self.shape[0]+1 , found+2*self.shape[0]]
+                newOrientation = 0
+        elif self.currentTetronome == 3:
+            if self.currentOrientation == 0:
+                oldPeice = [found,found+1,found+self.shape[0],found+self.shape[0]-1]
+                newPeice = [found-self.shape[0], found,found+1,found+self.shape[0]+1]
+                newOrientation = 1
+            elif self.currentOrientation == 1:
+                oldPeice = [found,found+self.shape[0],found+self.shape[0]+1,found+2*self.shape[0]+1]
+                if found%10 == 0 :
+                    found+=1
+                newPeice = [found+self.shape[0],found+self.shape[0]+1, found+2*self.shape[0],found+2*self.shape[0]-1 ]
+                newOrientation = 0
+        elif self.currentTetronome == 4:
+            if self.currentOrientation == 0:
+                oldPeice = [found,found+1,found+self.shape[0]+1,found+self.shape[0]+2]
+                newPeice = [found-self.shape[0]+2, found+2,found+1,found+self.shape[0]+1]
+                newOrientation = 1
+            elif self.currentOrientation == 1:
+                oldPeice = [found,found+self.shape[0],found+self.shape[0]-1,found+2*self.shape[0]-1]
+                if found%10 == 1 :
+                    found+=1
+                newPeice = [found+self.shape[0]-2,found+self.shape[0]-1, found+2*self.shape[0]-1,found+2*self.shape[0]]
+                newOrientation = 0
+        elif self.currentTetronome == 5:
+            if self.currentOrientation == 0:
+                newPeice = [found - self.shape[0] * 2 + 2, found - self.shape[0] + 2, found + 2,
+                            found + 2 + self.shape[0]]
+                oldPeice = [found, found + 1, found + 2, found + 3]
+                newOrientation = 1
+            elif self.currentOrientation == 1:
+                newPeice = [found + (2 * self.shape[0]) - 2, found + (2 * self.shape[0]) - 1,
+                            found + (2 * self.shape[0]), found + (2 * self.shape[0]) + 1]
+                oldPeice = [found, found + self.shape[0], found + 2 * self.shape[0], found + 3 * self.shape[0]]
+                newOrientation = 0
+                # edge case :
+                if found % 10 == 0:  # first column
+                    newPeice = [found + (2 * self.shape[0]) + 3, found + (2 * self.shape[0]) + 1,
+                                found + (2 * self.shape[0]) + 2, found + (2 * self.shape[0])]
+                elif found % 10 == 9:  # last column
+                    newPeice = [found + (2 * self.shape[0]) - 3, found + (2 * self.shape[0]) - 2,
+                                found + (2 * self.shape[0]) - 1, found + (2 * self.shape[0])]
+        elif self.currentTetronome == 6:
+            ret = False
+
+        if ret:
+            for i in range(4):
+                if self.board[newPeice[i]] == 2:
+                    ret = False
+            if move and ret:
+                for i in range(4):
+                    self.board[oldPeice[i]] = 0
+                for i in range(4):
+                    self.board[newPeice[i]] = 1
+                self.currentOrientation = newOrientation
+        return ret
 
     def Move(self, direction):
         if direction == "Down":
             for x in range(self.shape[0]):
-                for y in range(self.shape[1]-1, -1, -1):
+                for y in range(self.shape[1] - 1, -1, -1):
                     if self.board[x + y * self.shape[0]] == 1:
-                        self.board[x + (y+1) * self.shape[0]], self.board[x + y * self.shape[0]] = 1, 0
-        elif direction == "Right" :
-            for x in range(self.shape[0]-1, -1, -1):
+                        self.board[x + (y + 1) * self.shape[0]], self.board[x + y * self.shape[0]] = 1, 0
+        elif direction == "Right":
+            for x in range(self.shape[0] - 1, -1, -1):
                 for y in range(self.shape[1]):
                     if self.board[x + y * self.shape[0]] == 1:
-                        self.board[x+1 + y * self.shape[0]], self.board[x + y * self.shape[0]] = 1, 0
-        elif direction == "Left" :
+                        self.board[x + 1 + y * self.shape[0]], self.board[x + y * self.shape[0]] = 1, 0
+        elif direction == "Left":
             for x in range(self.shape[0]):
                 for y in range(self.shape[1]):
                     if self.board[x + y * self.shape[0]] == 1:
-                        self.board[x-1 + y * self.shape[0]], self.board[x + y * self.shape[0]] = 1, 0
+                        self.board[x - 1 + y * self.shape[0]], self.board[x + y * self.shape[0]] = 1, 0
 
     def SpawnATetronome(self):
-        self.currentTetronome = random.randint(0,6)
+        if len(self.spawnBag) ==0 :
+            self.spawnBag = [i for i in range(7)]
+            self.spawnBag.append(random.randint(0,6))
+            random.shuffle(self.spawnBag)
+
+        self.currentTetronome = self.spawnBag.pop(0)
+        self.currentOrientation = 0
         for i in self.tetronomeShape[self.currentTetronome]:
             if self.board[i] == 2:
                 print("Game Over")
@@ -96,17 +247,17 @@ class DisplayAbstraction():
                 quit()
             self.board[i] = 1
 
-
     def draw(self):
         for x in range(self.shape[0]):
             for y in range(self.shape[1]):
                 if self.board[x + y * self.shape[0]] == 1:
-                    colour = (255,0,0)
+                    colour = (255, 0, 0)
                 elif self.board[x + y * self.shape[0]] == 2:
-                    colour = (0,0,0)
+                    colour = (0, 0, 0)
                 else:
-                    colour = (255,255,255)
-                pygame.draw.rect(self.screen, colour,(x * 50, (y-2) * 50, x * 50 + 50, (y-2) * 50 + 50))
+                    colour = (255, 255, 255)
+                pygame.draw.rect(self.screen, colour, (x * 50, (y - 2) * 50, x * 50 + 50, (y - 2) * 50 + 50))
+
     def MoveCurrentBlockDown(self):
         if self.CheckLegality("Down"):
             self.Move("Down")
@@ -115,23 +266,25 @@ class DisplayAbstraction():
             self.SpawnATetronome()
 
     def checkRows(self):
-        for i in range (self.shape[1]-1,-1,-1):
+        for i in range(self.shape[1] - 1, -1, -1):
             full = True
-            for j in range (self.shape[0]):
+            for j in range(self.shape[0]):
                 if self.board[j + i * self.shape[0]] != 2:
                     full = False
             if full:
-                #print("Row " + str(i) + " is full")
+                # print("Row " + str(i) + " is full")
+                self.score += 1
+                print(f"The score is {self.score}")
                 self.deleterow(i)
 
-    def deleterow(self,row):
-        for row in range(row,0,-1):
+    def deleterow(self, row):
+        for row in range(row, 0, -1):
             for column in range(self.shape[0]):
-                self.board[column + row * self.shape[0]] = self.board[column + (row-1) * self.shape[0]]
+                self.board[column + row * self.shape[0]] = self.board[column + (row - 1) * self.shape[0]]
         for column in range(self.shape[0]):
             self.board[column] = 0
 
-    def run (self):
+    def run(self):
         counter = 0
         while (True):
             for event in pygame.event.get():
@@ -140,23 +293,21 @@ class DisplayAbstraction():
                     quit()
                 elif event.type == pygame.KEYDOWN:
                     self.Keypress(event.dict)
-            #print(pygame.time.get_ticks() / 1000)
+            # print(pygame.time.get_ticks() / 1000)
             if pygame.time.get_ticks() % (1000 // self.tickrate) == 0:
-                counter+=1
+                counter += 1
                 if counter == (1000 // self.tickrate):
+                    # print(counter)
                     counter = 0
-                    #print(time.time())
+                    # print(time.time())
                 if counter == 5:
                     self.MoveCurrentBlockDown()
                 self.checkRows()
                 self.draw()
-                #print(counter)
+                # print(counter)
             pygame.display.flip()
 
             self.clock.tick(self.tickrate)
-
-
-
 
 
 dis = DisplayAbstraction()
